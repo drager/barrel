ee675bd81f08e043f859c75b7db5c5ddfb296ac3
Jesper Håkansson <jesper@jesperh.se>
2017-10-08 22:31:35 +0200

    feat: Add fontIcon functions and function for rendering custom material icons

--- ui/src/Styles.elm
+++ ui/src/Styles.elm
@@ -3,8 +3,33 @@ module Styles exposing (..)
 import Css exposing (asPairs, px)
 import Html exposing (Html, Attribute, div)
 import Html.Attributes
+import Material.Icon
 
 
 styles : List Css.Style -> Attribute msg
 styles =
     Css.asPairs >> Html.Attributes.style
+
+
+type alias Icon =
+    { iconName : String
+    , iconSize : Int
+    , isCustomIcon : Bool
+    }
+
+
+customFontIcon : String -> Html msg
+customFontIcon iconName =
+    Html.i
+        [ Html.Attributes.class ("mdi mdi-" ++ iconName)
+        , styles [ Css.fontSize (Css.px 24) ]
+        ]
+        []
+
+
+fontIcon : Icon -> Html msg
+fontIcon { isCustomIcon, iconName, iconSize } =
+    if isCustomIcon then
+        customFontIcon iconName
+    else
+        Material.Icon.view iconName []
